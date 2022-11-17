50375bb387dac1414465af18651fa7b95c3e151c
Nezmet <nezmet@proton.me>
2022-11-16 22:21:26 -0500

    added convert.py. doesn't currently work right

---
+++ convert.py
@@ -0,0 +1,33 @@
+# Converter group project
+
+import sys
+
+class Unit:
+    def __init__(self, unitname, convertto, factor):
+        self.unitname = unitname
+        self.convertto = convertto
+        self.factor = factor
+
+def convert(s):
+    [from_, to_] = s.split(' to ')
+
+    for i in [Unit('kg',  ['lbs', 'oz', 'g'], [2.205, .028, 1000]),
+              Unit('lbs', ['oz',  'kg', 'g'], [16, .4535, .0022])]:
+        if from_.endswith(i.unitname):
+            val = float(from_.removesuffix(i.unitname))
+            ind = i.convertto.index(to_)
+
+    return f'{from_} is {val * i.factor[ind]:.1f}{i.convertto[ind]}'
+
+def parse(args):
+    result = ''
+
+    for arg in args:
+        result += arg + " "
+
+    return result.removesuffix(' ')
+
+if __name__ == "__main__":
+    val = parse(sys.argv[1:])
+    result = convert(val)
+    print(result)
